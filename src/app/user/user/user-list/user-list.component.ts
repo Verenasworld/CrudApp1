0865193c57e839617ca5c881c330af7d78e5c9b1
Verenasworld <verena.felizidas@gmail.com>
2021-06-09 14:44:52 +0200

    add transition on hover

--- src/app/user/user/user-list/user-list.component.ts
+++ src/app/user/user/user-list/user-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { UserListService } from './user-list.service';
 import { User } from '../../user';
 
